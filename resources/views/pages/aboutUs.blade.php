8f6a4f7d812b5355e322ce11b7a8c0ce30d4d644
Zé Diogo <zediogox@hotmail.com>
2022-11-14 23:29:43 +0000

    Started an aboutUs Page

---
+++ resources/views/pages/aboutUs.blade.php
@@ -0,0 +1,84 @@
+@extends('layout.app')
+
+@section('content_about')
+
+<div class="about-section">
+    <h1>About Us</h1>
+    <p>Some text about who we are and what we do.</p>
+</div>
+
+<h2 style="text-align:center">Our Team</h2>
+
+<div class="row">
+
+    <!-- AFONSO -->
+    <div class="column">
+        <div class="card">
+            <img src="" alt="" style="width:100%">
+            <div class="container">
+                <h2>Afonso Martins</h2>
+                <p class="title">Developer</p>
+                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                <p>
+                    <a href="mailto:up202005900@fe.up.pt">
+                        <i class="fa fa-envelope">up202005900@fe.up.pt</i>
+                    </a>
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <!-- EDU -->
+    <div class="column">
+        <div class="card">
+            <img src="" alt="" style="width:100%">
+            <div class="container">
+                <h2>Eduardo Silva</h2>
+                <p class="title">Developer</p>
+                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                <p>
+                    <a href="mailto:up202005283@fe.up.pt">
+                        <i class="fa fa-envelope">up202005283@fe.up.pt</i>
+                    </a>
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <!-- ZÉ -->
+    <div class="column">
+        <div class="card">
+            <img src="" alt="" style="width:100%">
+            <div class="container">
+                <h2>José Diogo</h2>
+                <p class="title">Developer</p>
+                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                <p>
+                    <a href="mailto:up202003529@fe.up.pt">
+                        <i class="fa fa-envelope">up202003529@fe.up.pt</i>
+                    </a>
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <!-- MATILDE -->
+
+    <div class="column">
+        <div class="card">
+            <img src="" alt="" style="width:100%">
+            <div class="container">
+                <h2>Matilde Silva</h2>
+                <p class="title">Developer</p>
+                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+
+                <p>
+                    <a href="mailto:up202007928@fe.up.pt">
+                        <i class="fa fa-envelope">up202007928@fe.up.pt</i>
+                    </a>
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
