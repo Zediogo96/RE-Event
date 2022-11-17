4d64c1dc2d0e7bc33eb1e812a8b4370bc19d03d2
matilde-silva-21 <matilde.m.silva.21@gmail.com>
2022-11-17 09:10:23 +0000

    mais umas mudanças no user

--- public/js/userPage.js
+++ public/js/userPage.js
@@ -1,24 +1,44 @@
 const selectOption = function(option) {
-    console.log(option)
     switch (option) {
         case 1: {
             document.getElementById('myProfileOption').classList.add('optionSelected');
             document.getElementById('myEventsOption').classList.remove('optionSelected');
             document.getElementById('myInvitesOption').classList.remove('optionSelected');
 
-            document.getElementById('myProfileDetails').classList.remove('userProfileDetailsHidden');
-            document.getElementById('myEventsDetails').classList.add('userProfileDetailsHidden');
-            document.getElementById('myInvitesDetails').classList.add('userProfileDetailsHidden');
+            document.getElementById('myProfileDetails').classList.remove('optionDetailsHidden');
+            document.getElementById('myEventsDetails').classList.add('optionDetailsHidden');
+            document.getElementById('myInvitesDetails').classList.add('optionDetailsHidden');
+
+            /*a parte de escolher submenus e esconder */
+            document.getElementById('myEventsSubmenu').classList.remove('submenuActive');
+            document.getElementById('myEventsSubmenu').classList.add('submenuSleep');
+
+            document.getElementById('myInvitesSubmenu').classList.remove('submenuActive');
+            document.getElementById('myInvitesSubmenu').classList.add('submenuSleep');
+
             break;
         }
         case 2: {
+            /*a parte de escolher profile, invite, submenu* e meter bold */
             document.getElementById('myProfileOption').classList.remove('optionSelected');
             document.getElementById('myEventsOption').classList.add('optionSelected');
             document.getElementById('myInvitesOption').classList.remove('optionSelected');
 
-            document.getElementById('myProfileDetails').classList.add('userProfileDetailsHidden');
-            document.getElementById('myEventsDetails').classList.remove('userProfileDetailsHidden');
-            document.getElementById('myInvitesDetails').classList.add('userProfileDetailsHidden');
+            /*a parte de dar display à option no lado direito */
+            document.getElementById('myProfileDetails').classList.add('optionDetailsHidden');
+            document.getElementById('myEventsDetails').classList.remove('optionDetailsHidden');
+            document.getElementById('myInvitesDetails').classList.add('optionDetailsHidden');
+
+            /*a parte de escolher submenus e esconder */
+            document.getElementById('myEventsSubmenu').classList.add('submenuActive');
+            document.getElementById('myEventsSubmenu').classList.remove('submenuSleep');
+
+            document.getElementById('myInvitesSubmenu').classList.remove('submenuActive');
+            document.getElementById('myInvitesSubmenu').classList.add('submenuSleep');
+
+            document.getElementById('pastEvents').classList.add('submenuActive');
+            document.getElementById('pastEvents').classList.remove('submenuSleep');
+
             break;
         }
         case 3: {
@@ -26,10 +46,114 @@ const selectOption = function(option) {
             document.getElementById('myEventsOption').classList.remove('optionSelected');
             document.getElementById('myInvitesOption').classList.add('optionSelected');
 
-            document.getElementById('myProfileDetails').classList.add('userProfileDetailsHidden');
-            document.getElementById('myEventsDetails').classList.add('userProfileDetailsHidden');
-            document.getElementById('myInvitesDetails').classList.remove('userProfileDetailsHidden');
+            document.getElementById('myProfileDetails').classList.add('optionDetailsHidden');
+            document.getElementById('myEventsDetails').classList.add('optionDetailsHidden');
+            document.getElementById('myInvitesDetails').classList.remove('optionDetailsHidden');
+
+            /*a parte de escolher submenus e esconder */
+            document.getElementById('myEventsSubmenu').classList.remove('submenuActive');
+            document.getElementById('myEventsSubmenu').classList.add('submenuSleep');
+
+            document.getElementById('myInvitesSubmenu').classList.add('submenuActive');
+            document.getElementById('myInvitesSubmenu').classList.remove('submenuSleep');
+
+            document.getElementById('receivedInvites').classList.add('submenuActive');
+            document.getElementById('receivedInvites').classList.remove('submenuSleep');
+
             break;
         }
     }
+}
+
+const showSubmenu = function(option){
+
+    switch(option){
+       case 1: {
+            document.getElementById('pastEvents').classList.add('submenuActive');
+            document.getElementById('pastEvents').classList.remove('submenuSleep');
+
+            document.getElementById('futureEvents').classList.remove('submenuActive');
+            document.getElementById('futureEvents').classList.add('submenuSleep');
+
+            document.getElementById('eventsCreatedByMe').classList.remove('submenuActive');
+            document.getElementById('eventsCreatedByMe').classList.add('submenuSleep');
+
+            document.getElementById('receivedInvites').classList.remove('submenuActive');
+            document.getElementById('receivedInvites').classList.add('submenuSleep');
+
+            document.getElementById('sentInvites').classList.remove('submenuActive');
+            document.getElementById('sentInvites').classList.add('submenuSleep');
+
+            break;
+        }
+        case 2: {
+            document.getElementById('pastEvents').classList.remove('submenuActive');
+            document.getElementById('pastEvents').classList.add('submenuSleep');
+
+            document.getElementById('futureEvents').classList.add('submenuActive');
+            document.getElementById('futureEvents').classList.remove('submenuSleep');
+
+            document.getElementById('eventsCreatedByMe').classList.remove('submenuActive');
+            document.getElementById('eventsCreatedByMe').classList.add('submenuSleep');
+
+            document.getElementById('receivedInvites').classList.remove('submenuActive');
+            document.getElementById('receivedInvites').classList.add('submenuSleep');
+
+            document.getElementById('sentInvites').classList.remove('submenuActive');
+            document.getElementById('sentInvites').classList.add('submenuSleep');
+            break;
+        } 
+        case 3: {
+            document.getElementById('pastEvents').classList.remove('submenuActive');
+            document.getElementById('pastEvents').classList.add('submenuSleep');
+
+            document.getElementById('futureEvents').classList.remove('submenuActive');
+            document.getElementById('futureEvents').classList.add('submenuSleep');
+
+            document.getElementById('eventsCreatedByMe').classList.add('submenuActive');
+            document.getElementById('eventsCreatedByMe').classList.remove('submenuSleep');
+
+            document.getElementById('receivedInvites').classList.remove('submenuActive');
+            document.getElementById('receivedInvites').classList.add('submenuSleep');
+
+            document.getElementById('sentInvites').classList.remove('submenuActive');
+            document.getElementById('sentInvites').classList.add('submenuSleep');
+            break;
+        } 
+        case 4: {
+            document.getElementById('pastEvents').classList.remove('submenuActive');
+            document.getElementById('pastEvents').classList.add('submenuSleep');
+
+            document.getElementById('futureEvents').classList.remove('submenuActive');
+            document.getElementById('futureEvents').classList.add('submenuSleep');
+
+            document.getElementById('eventsCreatedByMe').classList.remove('submenuActive');
+            document.getElementById('eventsCreatedByMe').classList.add('submenuSleep');
+
+            document.getElementById('receivedInvites').classList.add('submenuActive');
+            document.getElementById('receivedInvites').classList.remove('submenuSleep');
+
+            document.getElementById('sentInvites').classList.remove('submenuActive');
+            document.getElementById('sentInvites').classList.add('submenuSleep');
+            break;
+        }
+        case 5: {
+            document.getElementById('pastEvents').classList.remove('submenuActive');
+            document.getElementById('pastEvents').classList.add('submenuSleep');
+
+            document.getElementById('futureEvents').classList.remove('submenuActive');
+            document.getElementById('futureEvents').classList.add('submenuSleep');
+
+            document.getElementById('eventsCreatedByMe').classList.remove('submenuActive');
+            document.getElementById('eventsCreatedByMe').classList.add('submenuSleep');
+
+            document.getElementById('receivedInvites').classList.remove('submenuActive');
+            document.getElementById('receivedInvites').classList.add('submenuSleep');
+
+            document.getElementById('sentInvites').classList.add('submenuActive');
+            document.getElementById('sentInvites').classList.remove('submenuSleep');
+            break;
+        }  
+    }
+    
 }
--- resources/views/pages/userPage.blade.php
+++ resources/views/pages/userPage.blade.php
@@ -19,25 +19,55 @@
             imagem: John Doe 
         </div>
 
-        <div id = "selectOptions" class = "profile" >
-            <ul>
-                <li id="myProfileOption" class="optionSelected" onclick="selectOption(1)">My Profile</li>
-                <li id="myEventsOption" onclick="selectOption(2)">My Events</li>
-                <li id="myInvitesOption" onclick="selectOption(3)">My Invites</li>
-            </ul>  
+        <div id = "selectOptions">
+            <div id="myProfileOption" class="optionSelected option" onclick="selectOption(1)">My Profile</div>
+            
+            <div>
+                <div id="myEventsOption" class="option" onclick="selectOption(2)">My Events</div>
+                <div id="myEventsSubmenu" class = "submenuSleep" >
+                    <ul>
+                        <li onclick="showSubmenu(1)">Past Events</li>
+                        <li onclick="showSubmenu(2)">Future Events</li>
+                        <li onclick="showSubmenu(3)">Events Created By Me</li>
+                    </ul>  
+                </div>
+            </div>
+
+            <div>
+                <div id="myInvitesOption" onclick="selectOption(3)" class="option">My Invites</div>
+                <div id="myInvitesSubmenu" class = "submenuSleep">
+                    <ul>
+                        <li onclick="showSubmenu(4)">Received Invites</li>
+                        <li onclick="showSubmenu(5)">Sent Invites</li>
+                    </ul>  
+                </div>
+            </div>  
         </div>
     
 
     </section>
     <section id="selectDetails" class = "profile">
-        <div id="myProfileDetails" class="userProfileDetails">
+        <div id="myProfileDetails" class="optionDetails">
             myProfile
         </div>
-        <div id="myEventsDetails" class="userProfileDetails userProfileDetailsHidden">
-            myEvents
+        <div id="myEventsDetails" class="optionDetails optionDetailsHidden">
+            <div id="pastEvents" class="submenuSleep">
+                No past events :(
+            </div>
+            <div id="futureEvents" class="submenuSleep">
+                No future events :(
+            </div>
+            <div id="eventsCreatedByMe" class="submenuSleep">
+                No events created by you :(
+            </div>
         </div>
-        <div id="myInvitesDetails" class="userProfileDetails userProfileDetailsHidden">
-            myInvites
+        <div id="myInvitesDetails" class="optionDetails optionDetailsHidden">
+            <div id="receivedInvites" class="submenuSleep">
+                No invites Received :(
+            </div>
+            <div id="sentInvites" class="submenuSleep">
+                No invites sent :(
+            </div>
         </div>
         
     </section>
