e8042e557e28bacd6ee582936f9eb8804e49f457
Zé Diogo <zediogox@hotmail.com>
2022-12-13 23:29:20 +0000

    fix to createUserModal css

--- resources/views/partials/createUserModal.blade.php
+++ resources/views/partials/createUserModal.blade.php
@@ -8,29 +8,29 @@
             <div class="modal-body">
                 <form method='post' action="{{ route('storeUser')}}" enctype="multipart/form-data" id="_formCreateUser">
                     @csrf
-                    <div class="form-group mb-1">
+                    <div class="form-group mb-3">
                         <label for="name" class="form-label">User Name</label>
                         <input id="name" type="text" name="name" class="input-group form-control">
                         <div class="invalid-feedback"></div>
                     </div>
 
-                    <div class="form-group mb-1">
+                    <div class="form-group mb-3 form-inline">
                         <label for="email" class="form-label">Email</label>
                         <input id="email" type="text" name="email" class="input-group form-control">
                         <div class="invalid-feedback"></div>
 
                     </div>
-                    <div class="form-group mb-1">
+                    <div class="form-group mb-3">
                         <label for="birthdate" class="form-label">Birthdate</label>
                         <input id="birthdate" type="date" name="birthdate" class="input-group form-control">
                         <div class="invalid-feedback"></div>
                     </div>
-                    <div class="form-group mb-1">
+                    <div class="form-group mb-3">
                         <label for="password" class="form-label">Password</label>
                         <input id="password" type="password" name="password" value="" class="input-group form-control">
                         <div class="invalid-feedback"></div>
                     </div>
-                    <div class="form-group mb-1">
+                    <div class="form-group mb-3">
                         <label for="gender" class="form-label">Gender</label>
                         <input id="gender" type="text" name="gender" value="" class="input-group form-control">
                         <div class="invalid-feedback"></div>
@@ -42,7 +42,7 @@
                             <option value="O">Other</option>
                         </select> -->
                     </div>
-                    <div class="form-group mb-1">
+                    <div class="form-group mb-3">
                         <label for="profilePic" class="form-label">Profile Picture</label>
                         <input id="profilePic" type="file" name="profilePic" value="" class="input-group form-control">
                         <div class="invalid-feedback"></div>
