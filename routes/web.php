f9bf4d637f6680233dc310a519cf75d7d8ce86a3
Eduardo Silva <up202005283@up.pt>
2023-01-03 17:31:54 +0000

    password_resets table missing

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,13 @@
 |
 */
 
+//Extract to controller
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
 // Home
 Route::get('/', 'Auth\LoginController@home');
 
@@ -83,3 +90,53 @@ Route::get('auth', 'Auth\LoginController@getUser');
 
 Route::post('transferOwnership', 'EventController@transferOwnership')->name('transferOwnership'); //transfer the ownership of an event
 Route::post('deleteEvent', 'EventController@destroy')->name('deleteEvent'); //delete an event
+
+
+// Password Reset 
+
+Route::get('/forgot-password', function () {
+    return view('auth.recoverPass');
+})->middleware('guest')->name('password.request');
+
+Route::post('/forgot-password', function (Request $request) {
+    $request->validate(['email' => 'required|email']);
+
+    $status = Password::sendResetLink(
+        $request->only('email')
+    );
+
+    return $status === Password::RESET_LINK_SENT
+                ? back()->with(['status' => __($status)])
+                : back()->withErrors(['email' => __($status)]);
+})->middleware('guest')->name('password.email');
+
+Route::get('/reset-password/{token}', function ($token) {
+    return view('auth.reset-password', ['token' => $token]);
+})->middleware('guest')->name('password.reset');
+
+Route::post('/reset-password', function (Request $request) {
+    $request->validate([
+        'token' => 'required',
+        'email' => 'required|email',
+        'password' => 'required|min:8|confirmed',
+    ]);
+
+    $status = Password::reset(
+        $request->only('email', 'password', 'password_confirmation', 'token'),
+        function ($user, $password) {
+            $user->forceFill([
+                'password' => Hash::make($password)
+            ])->setRememberToken(Str::random(60));
+
+            $user->save();
+
+            event(new PasswordReset($user));
+        }
+    );
+
+    return $status === Password::PASSWORD_RESET
+                ? redirect()->route('login')->with('status', __($status))
+                : back()->withErrors(['email' => [__($status)]]);
+})->middleware('guest')->name('password.update');
+
+// $schedule->command('auth:clear-resets')->everyFifteenMinutes();
